8bf6838eff4153fdb0d0f69b0c3b8432a0a65869
danielriory173 <danielriory173@gmail.com>
2025-10-02 16:22:56 +0700

    edit

--- Kasir.cpp
+++ Kasir.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 const int MAX_ITEMS = 100;
 
+void availableItems(string items[], int jumlahItem[], double harga[]) {
+    items[0] = "Mie Indomie";
+    harga[0] = 3000;
+}
+
 int main(){
     cout << "Hello, World! saya suka C++" << endl;
     cout << "Emg iyah? - Daniel" << endl;
