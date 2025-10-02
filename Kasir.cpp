f081d5769391fe6c4edb71ff388ababb09ca883d
Bita <Nailahtsabitah3012@gmail.com>
2025-10-02 15:20:49 +0700

    Update Kasir.cpp

--- Kasir.cpp
+++ Kasir.cpp
@@ -4,7 +4,12 @@ using namespace std;
 const int MAX_ITEMS = 100;
 
 int main(){
-    cout << "Hello, World! saya suka C++" << endl;
-    cout << "Emg iyah? - Daniel" << endl;
+
+
+
+
+
+
+    
     return 0; 
 }
