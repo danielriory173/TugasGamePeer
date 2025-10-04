c426ca6d4a0223d77a4f41742141f6024007b940
aksaaa <akbarrajasa2@gmail.com>
2025-10-04 20:14:08 +0700

    Update Kasir.cpp

--- Kasir.cpp
+++ Kasir.cpp
@@ -31,7 +31,7 @@ void availableItems(string items[], int jumlahItem[], double harga[]) {
     items[8] = "Minyak 1L";
     harga[8] = 20000;
 
-
+//fungsi buat input barangnya
     for (int i = 0; i < 9; i++) {
         cout << "Masukan jumlah " << items[i] << ": ";
         cin >> jumlahItem[i];
@@ -39,12 +39,39 @@ void availableItems(string items[], int jumlahItem[], double harga[]) {
 
 }
 
+//fungsi buat hitung totalnya
+double hitungTotal(int jumlahItem[], double harga[],int x){
+        double total = 0;
+        for (int i=0; i<x; i++){
+            total = total + (jumlahItem[i]*harga[i]);
+        }
+            return total;
+    }
+
 int main(){
 
+    cout<<endl;
+    cout<<"List Daftar Barang Belanjaan yang Tersedia : "<<endl;
+    cout<<"1. Indomie           = Rp 3.000"<<endl;
+    cout<<"2. Tepung Terigu     = Rp 9.000"<<endl;
+    cout<<"3. Telur 1 Kg        = Rp 22.000"<<endl;
+    cout<<"4. Gula              = Rp 16.000"<<endl;
+    cout<<"5. Garam             = Rp 10.000"<<endl;
+    cout<<"6. Kopi              = Rp 15.000"<<endl;
+    cout<<"7. Beras 5 Kg        = Rp 150.000"<<endl;
+    cout<<"8. Galon             = Rp 20.000"<<endl;
+    cout<<"9. Minyak 1 L        = Rp 20.000"<<endl;
+    cout<<endl;
 
+    string items[ MAX_ITEMS];
+    int jumlahItem[ MAX_ITEMS];
+    double harga[ MAX_ITEMS];
+    availableItems(items,jumlahItem,harga);
+    double total = hitungTotal(jumlahItem,harga,9);
 
-
-
+    cout<<endl;
+    cout<<"Total Bayar = Rp "<<total;
+    cout<<endl;
 
     
     return 0; 
